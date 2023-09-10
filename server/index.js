148a759b5558137ad72123058ecf2f5567dc02b8
viktoriiakoniukhova <viktoriia.koniukhova@gmail.com>
2023-09-10 13:05:44 +0300

    set cors manually

--- server/index.js
+++ server/index.js
@@ -11,14 +11,33 @@ const bodyParser = require("body-parser");
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const cookieParser = require("cookie-parser");
 
-app.use(
-  cors({
-    origin: ["https://mern-tester-front.vercel.app/"],
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
-    sameSite: "none",
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["https://mern-tester-front.vercel.app/"],
+//     methods: ["GET", "POST", "DELETE", "PUT"],
+//     credentials: true,
+//     sameSite: "none",
+//   })
+// );
+
+// Set middleware of CORS
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://mern-tester-front.vercel.app/"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
