5c90f99a67a2a62de0f47f7ad4c38560d3c347d2
viktoriiakoniukhova <viktoriia.koniukhova@gmail.com>
2023-09-20 14:56:22 +0300

    fix: client url fixed

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -1,2 +1,2 @@
 export const base_url = "https://mern-tester-server.onrender.com/api/";
-export const nav_url = "https://mern-tester-front.vercel.app/";
+export const nav_url = "https://mern-tester-front.vercel.app";
